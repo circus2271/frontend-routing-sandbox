9e951ffc8e0da5dfc8d52155cfab78d9a8994c69
circus2271 <rodionov134@gmail.com>
2023-08-10 18:08:47 +0300

    extract backButton into a component

--- src/assets/scripts/custom-elements.ts
+++ src/assets/scripts/custom-elements.ts
@@ -201,3 +201,49 @@ class AvailableAlbums extends HTMLElement {
 }
 
 customElements.define('available-albums', AvailableAlbums)
+
+class BackButton extends HTMLElement {
+  anchor: HTMLAnchorElement
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.anchor = this.querySelector('a')
+    this.updateUi();
+
+    window.addEventListener(urlChange, this.onUrlChange)
+  }
+
+  updateUi = () => {
+
+    const urlParts = location.pathname.split('/').filter(part => part !== '');
+
+    if (urlParts.length === 0) {
+      this.classList.remove('visible')
+      this.anchor.setAttribute('href', '/')
+    } else if (urlParts.length === 1) {
+      this.classList.add('visible')
+      this.anchor.setAttribute('href', '/')
+    } else {
+      this.classList.add('visible')
+
+      // slice returns new array (from start, to end (not included))
+      const newUrl = urlParts.slice(0, urlParts.length - 1 ).join('');
+      this.anchor.setAttribute('href', '/' + newUrl)
+      // const lastUrlPart = urlParts[urlParts.length - 1]
+      // const newUrl =
+    }
+  }
+
+  onUrlChange = () => {
+    this.updateUi()
+  }
+
+  disconnectedCallback() {
+    window.removeEventListener(urlChange, this.onUrlChange)
+  }
+}
+
+customElements.define('back-button', BackButton)
