5974121c30faf342ee9a75db7f9452500fb98439
circus2271 <rodionov134@gmail.com>
2023-08-16 04:54:28 +0300

    modify errors

--- src/assets/scripts/modules/custom-errors.ts
+++ src/assets/scripts/modules/custom-errors.ts
@@ -2,22 +2,20 @@
 
 export class GroupNotFoundError extends Error {
   constructor(message?) {
-    // super(message);
     super();
 
     this.name = 'GroupNotFoundError'
-    this.message = 'group not found, 404'
+    this.message = message || 'group not found, 404'
   }
 }
 
 
 export class AlbumNotFoundError extends Error {
   constructor(message?) {
-    // super(message);
     super();
 
     this.name = 'AlbumNotFoundError'
-    this.message = 'album not found, 404'
+    this.message = message || 'album not found, 404'
   }
 }
 
