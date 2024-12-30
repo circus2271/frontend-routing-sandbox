b9ee36cb5dce5a7c3abe6a7ab2f3f61b58afe9e9
circus2271 <p@WIN-R6PSOGHCMGP.localdomain>
2024-12-30 12:18:04 +0300

    improve navigation

--- src/assets/scripts/index.ts
+++ src/assets/scripts/index.ts
@@ -6,13 +6,24 @@ type Route = {
     relativePath: string,
     onNavigate?: () => void,
     canNavigate: () => boolean,
-    redirectTo?: string
+    redirectTo?: string,
+    beforeNavigate?: () => Promise<void>,
+    beforeLeave?: () => void
 }
 
 const routes: Route[] = [
     {
         relativePath: '/',
         canNavigate: () => true
+    },
+    {
+        relativePath: '/about',
+        canNavigate: () => true
+    },
+    {
+        relativePath: '/with-slider',
+        canNavigate: () => true,
+        beforeNavigate: async () => alert('initialize slider')
     },
     {
         relativePath: '/group10/album-1',
@@ -24,9 +35,10 @@ const routes: Route[] = [
 
 class Router {
     routes: Route[] = routes
+    beforeNavigateFunctionsStack: (() => Promise<void>)[] = []
 
     constructor() {
-        document.addEventListener('click', e => {
+        document.addEventListener('click', async (e) => {
             if (e.target instanceof HTMLAnchorElement) {
                 if (e.target.hasAttribute('custom-link')) {
                     e.preventDefault()
@@ -38,18 +50,40 @@ class Router {
 
                     // nextRoute.redirectTo = '/about'
                     if (!canNavigate) {
-                        const r = nextRoute.redirectTo
+                        // const r = nextRoute.redirectTo
                         setTimeout(() => {
                             // location.pathname = r
                             history.pushState({}, '', nextRoute.redirectTo)
 
                         }, 2000)
 
+                        return
                     }
+
+                    // this.beforeNavigateFunctionsStack.forEach(f => f())
+                    for await (const callback of this.beforeNavigateFunctionsStack) {
+                        await callback()
+                    }
+                    await nextRoute.beforeNavigate?.()
+                    history.pushState({}, '', nextRoute.relativePath)
                 }
             }
         })
     }
+
+    beforeNavigate() {
+        this.beforeNavigateFunctionsStack.forEach(f => f())
+        // this.routes.forEach(route => route.beforeNavigate())
+    }
+
+    // register before navigation callbacks
+    addFunctionToBeforeNavigateFunctionStack(func: () => Promise<void>) {
+        this.beforeNavigateFunctionsStack.push(func)
+    }
 }
 
 export const router = new Router()
+
+router.addFunctionToBeforeNavigateFunctionStack(async () => {
+    window.scrollTo({ top: 0, behavior: 'smooth'})
+})
